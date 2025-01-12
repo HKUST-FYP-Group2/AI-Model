31c47c4f8f067f680ac0eb3694e91156532de038
hvgupta <hvgupta@outlook.in>
2025-01-12 14:46:31 +0800

    added a dataclass

--- getData.py
+++ getData.py
@@ -2,35 +2,47 @@ import os
 import dotenv
 import requests
 import json
-from dataclasses import dataclass
+from dataclasses import dataclass, replace, asdict
 
-@dataclass
+@dataclass(frozen=True)
 class WeatherData:
     temperature: float
     humidity: float
     wind_speed: float
-    wind_direction: float
-    weather_description: str
-    weather_icon: str
-    sunrise: str
-    sunset: str
+    cloud_cover: float
+    current_time: int
+    visibility: int
+    gust: float
+    rain_1h:float
+    snow_1h:float
+    
 
 
 GET_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
 GET_WEBCAM_URL = "https://api.windy.com/webcams/api/v3/webcams"
 
-def getRelaventData(weatherData)->dict:
-    pass
+def getRelaventData(weatherData:dict)->WeatherData:
+    temperature = float(weatherData["main"]["temp"])
+    humidity = float(weatherData["main"]["humidity"])
+    wind_speed = float(weatherData["wind"]["speed"])
+    cloud_cover = float(weatherData["clouds"]["all"])
+    current_time = int(weatherData["dt"])
+    visibility = int(weatherData["visibility"])
+    gust = float(weatherData.get("wind", {}).get("gust", 0.0))
+    rain_1h = float(weatherData.get("rain", {}).get("1h", 0.0))
+    snow_1h = float(weatherData.get("snow", {}).get("1h", 0.0))
+    return WeatherData(temperature, humidity, wind_speed, cloud_cover, current_time, visibility, gust, rain_1h, snow_1h)
 
-def getData():
+def getData(params:dict):
     openWeather_api_key = os.getenv("openWeather_api_key")
     Windy_webCam_api_key = os.getenv("Windy_webCam_api_key")
     
-    response = requests.get(GET_WEATHER_URL, params={"q": "Hong Kong", "appid": openWeather_api_key})
+    __param = dict(params,**{"appid":openWeather_api_key})
+    response = requests.get(GET_WEATHER_URL, params=__param)
     if response.status_code != 200:
         raise ValueError(response.reason)
     weather_data = response.content
-    
+    weatherInfo = getRelaventData(json.loads(weather_data))
     with open("test_weather.json", "w") as f:
         json.dump(response.json(), f)
     
@@ -42,7 +54,7 @@ if __name__ == "__main__":
             raise ValueError("No OpenWeatherMap API key found in .env file")
         if os.getenv("Windy_webCam_api_key") is None:
             raise ValueError("No Windy.com API key found in .env file")
-        getData()
+        getData({"q":"Hong Kong"})
     else:
         raise FileNotFoundError("No .env file found, create your own for OpenWeatherMap API key and Windy.com API key (Webcam API)")
     
