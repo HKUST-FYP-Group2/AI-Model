9cb2bc44c2c03f12bbe5bf790c34250b996b20ff
hvgupta <hvgupta@outlook.in>
2025-01-31 14:21:43 +0800

    env load test

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,13 @@ WORKDIR /model
 # Copy the requirements file
 COPY requirements.txt .
 
+RUN --mount=type=secret,id=openWeather_api_key\
+    --mount=type=secret,id=Windy_webCam_api_key\
+    export openWeather_api_key=$(cat /run/secrets/openWeather_api_key) &&\
+    export Windy_webCam_api_key=$(cat /run/secrets/Windy_webCam_api_key)
+
+RUN env | grep openWeather_api_key
+RUN env | grep Windy_webCam_api_key
 # Install the dependencies
 RUN pip install --no-cache-dir -r requirements.txt
 
--- getData.py
+++ getData.py
@@ -2,31 +2,6 @@ import os
 import dotenv
 from dataset.Create_Dataset import CreateDataset
 
-GET_WEATHER_URL = "https://api.openweathermap.org/data/2.5/weather"
-GET_WEBCAM_URL = "https://api.windy.com/webcams/api/v3/webcams"
-
-
-# def checkAndDownloadImg(response: dict):
-#     for webcamId in response:
-#         response = requests.get(
-#             f"https://api.windy.com/webcams/api/v3/webcams/{webcamId}", headers={"x-windy-api-key": os.getenv("Windy_webCam_api_key")}, params={"include": "images"})
-#         img_link = response.json()["images"]["current"]["preview"]
-#         a, b = urllib.request.urlretrieve(img_link, f"{webcamId}.jpg")
-
-
-# def getData(params: dict):
-#     openWeather_api_key = os.getenv("openWeather_api_key")
-#     Windy_webCam_api_key = os.getenv("Windy_webCam_api_key")
-
-#     openWeatherFetcher = WeatherClient(
-#         mode="json", units="metric", lang="en")
-#     weatherInfo, weather_status = openWeatherFetcher.get_weather_info(
-#         openWeather_api_key, location=params["q"])
-
-#     windyFetcher, _ = WebCamClient(10, 250.0, "./dataset")
-#     success = windyFetcher.downloadImages(
-#         key=Windy_webCam_api_key, lat=weatherInfo.lat, lon=weatherInfo.lon)
-
 
 def createDataset():
     Manager = CreateDataset()
