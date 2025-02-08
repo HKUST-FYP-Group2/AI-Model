d200df35e24ec17c9c65fcb592f52c9f4095f5af
hvgupta <hvgupta@outlook.in>
2025-02-09 00:32:16 +0800

    fixed the logger path

--- Logger/Logger.py
+++ Logger/Logger.py
@@ -1,7 +1,10 @@
 import logging
+import os
+
+curDirPath = os.path.dirname(__file__)
 
 # Configure the logger
 logging.basicConfig(
-    filename="AI_logger.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s",
+    filename=f"{curDirPath}/../AI_logger.log", filemode="w", format="%(asctime)s - %(levelname)s - %(message)s",
     level=logging.INFO)
 logger = logging.getLogger(__name__)
